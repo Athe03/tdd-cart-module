fb53357fae31f732e941d31468315e83d37639d2
Athe03 <gravilathenais@free.fr>
2025-04-30 13:02:44 +0200

    fonction + test add, remove, count

--- src/cart.ts
+++ src/cart.ts
@@ -5,4 +5,32 @@ export type Product = {
   quantity: number;
 };
 
-// TODO: implémenter le module cart ici
+export class Cart {
+  products: Product[] = [];
+
+  constructor() {
+    this.products = [];
+  }
+
+  addProduct(product: Product): void {
+    let exist = false;
+
+    for (let i = 0; i< this.products.length; i ++) {
+      if (this.products[i].id === product.id) {
+        exist = true;
+      }
+    }
+
+    if(exist === false) {
+      this.products.push(product);
+    }
+  }
+
+  removeProduct(productId: string): void {
+    this.products.splice(this.products.findIndex(p => p.id === productId), 1);
+  }
+
+  getProductCount(): number {
+    return this.products.length;
+  }
+}
--- tests/cart.test.ts
+++ tests/cart.test.ts
@@ -1,10 +1,73 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, test, beforeEach } from "vitest";
+import { Product, Cart } from "../src/cart";
+
+
+const product: Product = {
+  id: "1",
+  name: "Product 1",
+  price: 10,
+  quantity: 1,
+}
+
+const product2: Product = {
+  id: "2",
+  name: "Product 2",
+  price: 5,
+  quantity: 7,
+}
+
+let cart: Cart = new Cart();
 
 describe("cart module", () => {
-  it("should add a product to the cart", () => {
-    // écrire le test ici
-    expect(true).toBe(true);
-  });
-});
+  beforeEach(() => {
+    cart = new Cart();
+  })
 
-// TODO : ajouter d'autres tests pour le module cart
+  //Test add product
+  test("should add a product", () => {
+    expect(cart.getProductCount()).toEqual(0);
+    
+    cart.addProduct(product);
+
+    expect(cart.getProductCount()).toEqual(1);
+  })
+
+  test("should add an existing product", () => {
+    expect(cart.getProductCount()).toEqual(0);
+    
+    cart.addProduct(product);
+    cart.addProduct(product);
+
+    expect(cart.getProductCount()).toEqual(1);
+  })
+
+  // Test remove product
+  test("should remove a product", () => {
+    cart.addProduct(product);
+    
+    expect(cart.getProductCount()).toEqual(1);
+
+    cart.removeProduct(product.id);
+
+    expect(cart.getProductCount()).toEqual(0);
+  })
+
+  test("should remove a not existent product", () => {
+    expect(cart.getProductCount()).toEqual(0);
+
+    cart.removeProduct(product.id);
+
+    expect(cart.getProductCount()).toEqual(0);
+  })
+
+  //Test the total number of products
+  test("should calculate the total number of products", () => {
+    expect(cart.getProductCount()).toEqual(0);
+
+    cart.addProduct(product);
+    cart. addProduct(product2);
+    cart.removeProduct(product.id);
+
+    expect(cart.getProductCount()).toEqual(1);
+  })
+});
